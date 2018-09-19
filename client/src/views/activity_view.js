95f516f836e80fa10f2f0ca6d35824589a3be1fd
Digory <digory1@googlemail.com>
2018-09-19 15:45:35 +0100

    Added 'put' method to request

--- client/src/views/activity_view.js
+++ client/src/views/activity_view.js
@@ -1,5 +1,10 @@
+const Request = require('../helpers/request.js');
+const PubSub = require('../helpers/pub_sub.js');
+
 const ActivityView = function(mainContainer){
     this.mainContainer = mainContainer;
+    this.url = 'http://localhost:3000/api/activities';
+    this.request = new Request(this.url);
 }
 
 ActivityView.prototype.render = function(activity){
@@ -16,8 +21,10 @@ ActivityView.prototype.render = function(activity){
     const doneContainer = this.createDetail(determineContentOfDone);
     container.appendChild(doneContainer);
 
-
-
+    if(activity.done === "false"){
+        const doneButton = this.createButton(activity);
+        container.appendChild(doneButton);
+    }
     this.mainContainer.appendChild(container);
 }
 
@@ -31,6 +38,26 @@ ActivityView.prototype.createDetail = function(textContent){
     const detail = document.createElement('p');
     detail.textContent = textContent;
     return detail;
+}
+
+ActivityView.prototype.createButton = function(activity){
+    console.log(activity._id);
+    const id = activity._id;
+    const button = document.createElement('button');
+    button.textContent = "&#x2714";
+    button.addEventListener('click', (event)=>{
+        const newActivityDetails = {
+            name: activity.name,
+            location: activity.location,
+            done: "true"
+        }
+        this.request.put(newActivityDetails, id)
+        .then((activities)=>{
+            PubSub.publish('Bucket:all-data-loaded', activities);
+        })
+        .catch(console.error);
+    });
+    return button;
 }
 
 ActivityView.prototype.determineDone = function(stringBoolean){
--- server/db/seeds.js
+++ server/db/seeds.js
@@ -5,19 +5,16 @@ db.activities.insertMany([
     {
         name: "Skydiving",
         location: "Romania",
-        price: "500",
-        done: true
+        done: "true"
     },
     {
         name: "Whalewatching",
         location: "Iceland",
-        price: "300",
-        done: false
+        done: "false"
     },
     {
         name: "Blow the whale horn of the guy who has the whale horn",
         location: "South Africa",
-        price: "5000",
-        done: true
+        done: "true"
     }
 ])
