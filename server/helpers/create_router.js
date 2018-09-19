fe90e3c15a2c5f962dd006b06d82e4b23fa1fcab
Digory <digory1@googlemail.com>
2018-09-19 11:51:42 +0100

    Tests passed for UPDATE route

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -7,13 +7,13 @@ const CreateRouter = function(collection){
     // INDEX
     router.get('/', (req,res)=>{
         collection
-        .find()
-        .toArray()
-        .then((docs)=>res.json(docs))
-        .catch((err) => {
-            console.error(err);
-            res.status(500);
-            res.json({ status: 500, error: err });
+            .find()
+            .toArray()
+            .then((docs)=>res.json(docs))
+            .catch((err) => {
+                console.error(err);
+                res.status(500);
+                res.json({ status: 500, error: err });
           });
     });
 
@@ -21,13 +21,13 @@ const CreateRouter = function(collection){
     router.get('/:id', (req,res)=>{
         const id = req.params.id;
         collection
-        .find({_id: ObjectID(id)})
-        .toArray()
-        .then((docs)=>res.json(docs))
-        .catch((err) => {
-            console.error(err);
-            res.status(500);
-            res.json({ status: 500, error: err });
+            .find({_id: ObjectID(id)})
+            .toArray()
+            .then((docs)=>res.json(docs))
+            .catch((err) => {
+                console.error(err);
+                res.status(500);
+                res.json({ status: 500, error: err });
           });
     });
 
@@ -35,18 +35,31 @@ const CreateRouter = function(collection){
     router.post('/', (req,res)=>{
         const newData = req.body;
         collection
-        .insertOne(newData)
-        .then(()=>collection.find().toArray())
-        .then((docs)=>res.json(docs));
+            .insertOne(newData)
+            .then(()=>collection.find().toArray())
+            .then((docs)=>res.json(docs));
     });
 
     // DESTROY
     router.delete('/:id', (req,res)=>{
         const id = req.params.id;
         collection
-        .deleteOne({_id: ObjectID(id)})
-        .then(()=>collection.find().toArray())
-        .then((docs)=>res.json(docs));
+            .deleteOne({_id: ObjectID(id)})
+            .then(()=>collection.find().toArray())
+            .then((docs)=>res.json(docs));
+    });
+
+    // UPDATE
+    router.put('/:id', (req,res)=>{
+        const id = req.params.id;
+        const updatedData = req.body;
+        collection
+            .updateOne(
+                {_id: ObjectID(id)},
+                {$set: updatedData}
+            )
+            .then(()=>collection.find().toArray())
+            .then((docs)=>res.json(docs));
     });
 
     return router;
