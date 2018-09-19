18feff0373d618c8b1d6fdaf9464bde4b7c35530
Digory <digory1@googlemail.com>
2018-09-19 11:42:47 +0100

    Test passed for DESTROY route

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -5,16 +5,19 @@ db.activities.insertMany([
     {
         name: "Skydiving",
         location: "Romania",
-        price: "500"
+        price: "500",
+        done: true
     },
     {
         name: "Whalewatching",
         location: "Iceland",
-        price: "300"
+        price: "300",
+        done: false
     },
     {
         name: "Blow the whale horn of the guy who has the whale horn",
         location: "South Africa",
-        price: "5000"
+        price: "5000",
+        done: true
     }
 ])
--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ObjectID = require('mongod').ObjectID;
+const ObjectID = require('mongodb').ObjectID;
 
 const CreateRouter = function(collection){
     const router = express.Router();
@@ -9,10 +9,47 @@ const CreateRouter = function(collection){
         collection
         .find()
         .toArray()
+        .then((docs)=>res.json(docs))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error: err });
+          });
+    });
+
+    // SHOW
+    router.get('/:id', (req,res)=>{
+        const id = req.params.id;
+        collection
+        .find({_id: ObjectID(id)})
+        .toArray()
+        .then((docs)=>res.json(docs))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error: err });
+          });
+    });
+
+    // CREATE
+    router.post('/', (req,res)=>{
+        const newData = req.body;
+        collection
+        .insertOne(newData)
+        .then(()=>collection.find().toArray())
         .then((docs)=>res.json(docs));
-    })
+    });
+
+    // DESTROY
+    router.delete('/:id', (req,res)=>{
+        const id = req.params.id;
+        collection
+        .deleteOne({_id: ObjectID(id)})
+        .then(()=>collection.find().toArray())
+        .then((docs)=>res.json(docs));
+    });
 
     return router;
-}
+};
 
 module.exports = CreateRouter;
